be56bc6c76adfb58c88319df5c2a5af4f04e00fa
yasun <yasun@LAPTOP-FCFAN22B>
2020-09-12 18:43:49 +0900

    commit

---
+++ src/main/java/mavenSample/Main01.java
@@ -0,0 +1,15 @@
+package mavenSample;
+
+public class Main01 {
+	public String getColor(int i) {
+
+        if (i == 1) {
+            return "赤";
+        } else if (i == 2) {
+            return "青";
+
+        } else {
+            return "1or2を入力してください";
+        }
+    }
+}
---
+++ src/test/java/mavenSample/Test01.java
@@ -0,0 +1,27 @@
+package mavenSample;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Test;
+
+public class Test01 {
+	@Test
+    public void testGetColor1() {
+        Main01 c1 = new Main01();
+        String t1 = c1.getColor(1);
+        assertThat(t1,is("赤"));
+    }
+    @Test
+    public void testGetColor2() {
+    	Main01 c1 = new Main01();
+        String t1 = c1.getColor(2);
+        assertThat(t1,is("青"));
+    }
+    @Test
+    public void testGetColor3() {
+    	Main01 c1 = new Main01();
+        String t1 = c1.getColor(3);
+        assertThat(t1,is("1or2を入力してください"));
+    }
+}
